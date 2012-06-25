cb02a08d552d4485c0bb67f9df5a37d21a78a025
PlasmaLampe <jorgamelunxen@noname.(none)>
2012-06-25 16:58:46 +0200

    minor bugfix

--- download_extern.php
+++ download_extern.php
@@ -3,42 +3,48 @@
 
 <html lang="en">
 <head>
+	<?php
+		$conference = $_POST['conference'];
+		$result = mysql_query("SELECT id FROM event WHERE text='$conference'");
+		$row = mysql_fetch_object($result);
+		$idConferenc = $row->id;
+
+		$syear = $_POST['syear'];
+		$eyear = $_POST['eyear'];
+	?>
+
 	<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
 	<title>untitled</title>
 	<meta name="generator" content="TextMate http://macromates.com/">
 	<meta name="author" content="Jörg Amelunxen">
+	
 	<!-- Date: 2012-06-21 -->
 	
 	<?php
 		include("include/db.php"); // .htaccess secured :)
 	?>
+
+	<?php
+		if($_POST['chosenmetric'] == "co-authorship"){
+		$remoteURL = "http://$USER:$PASSWORD@mlearn.aan.cs.upb.de/Export/CoAuthorGexf?startyear=$syear&endyear=$eyear&uploaded=true&eventid=$idConferenc";
+
+		// download file to server
+		file_put_contents("data/$conference.gexf", file_get_contents($remoteURL));
+
+		echo "your file has been downloaded, click <a href='vizz_neu?url=data/$conference.gexf'>here</a> to continue ...";
+		}elseif($_POST['chosenmetric'] == "co-citation"){
+		$remoteURL = "http://84.200.8.141:8080/GEXFServer/Servlet?eventid=$idConferenc&graphtype=cc&syear=$syear&eyear=$eyear";
+		echo $remoteURL;
+		$link = file_get_contents($remoteURL); 
+		echo '<meta http-equiv="refresh" content="3; URL=$link">';
+		}elseif($_POST['chosenmetric'] == "bibliographic coupling"){
+		echo "sorry, this is a todo feature ... :( ...";
+		}
+	?>
 	
 </head>
 <body>
-<?php
-$conference = $_POST['conference'];
-$result = mysql_query("SELECT id FROM event WHERE text='$conference'");
-$row = mysql_fetch_object($result);
-$idConferenc = $row->id;
+please wait ...
 
-$syear = $_POST['syear'];
-$eyear = $_POST['eyear'];
-
-if($_POST['chosenmetric'] == "co-authorship"){
-$remoteURL = "http://$USER:$PASSWORD@mlearn.aan.cs.upb.de/Export/CoAuthorGexf?startyear=$syear&endyear=$eyear&uploaded=true&eventid=$idConferenc";
-
-// download file to server
-file_put_contents("data/$conference.gexf", file_get_contents($remoteURL));
-
-echo "your file has been downloaded, click <a href='vizz_neu?url=data/$conference.gexf'>here</a> to continue ...";
-}elseif($_POST['chosenmetric'] == "co-citation"){
-$remoteURL = "http://84.200.8.141:8080/GEXFServer/Servlet?eventid=$idConferenc&graphtype=cc";
-echo $remoteURL;
-echo file_get_contents($remoteURL); 
-
-}elseif($_POST['chosenmetric'] == "bibliographic coupling"){
-echo "sorry, this is a todo feature ... :( ...";
-}
-?>
 </body>
 </html>
