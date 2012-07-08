70075e2c32240235d234dc6ee86c5cb7e134bf0d
PlasmaLampe <jorgamelunxen@noname.(none)>
2012-07-08 13:21:11 +0200

    minor changes

--- circos.php
+++ circos.php
@@ -60,7 +60,7 @@
 		<?php
 		$cc_pic = file_get_contents("http://84.200.8.141:8080/GEXFServer/Servlet?url=".$_GET['url']."&metric=cc&circos=true"); 
 		echo "<h3> Closeness centrality </h3>";
-		echo "<p><img src=\"".$cc_pic."\" width=\"700\" height=\"700\"></p>";
+		echo "<p><a href=\"".$cc_pic."\"><img src=\"".$cc_pic."\" width=\"700\" height=\"700\"></a></p>";
 		?>
 		
 		
