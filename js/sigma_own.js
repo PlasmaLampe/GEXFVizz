fad841ff18a2e3eed2b8a78fc0b89351533af3ed
PlasmaLampe <jorgamelunxen@Jorg-Amelunxens-MacBook-Pro-3.local>
2012-07-16 21:43:50 +0200

    bugfix

--- js/sigma_own.js
+++ js/sigma_own.js
@@ -217,11 +217,12 @@ function init() {
 	},true);
 	
 	function updateButtonLabel(id,newvalue){
-		if(slider.getValue() != 0 && newvalue != -1){
-			var finalvalue = "show "+slider.getValue()+mindate + newvalue;
-			document.getElementById(id).value=finalvalue;
-		}else{
+		if(slider.getValue() == 0 && newvalue == -1){
 			document.getElementById(id).value="N/A";
+		}else{
+			var newcalcDate = slider.getValue() + mindate + newvalue;
+			var finalvalue = "show "+newcalcDate;
+			document.getElementById(id).value=finalvalue;
 		}	
 	}
 	
