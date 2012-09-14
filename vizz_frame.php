db160b613f40ea71f2162f305df9eff4893881f1
PlasmaLampe <jorgamelunxen@Jorg-Amelunxens-MacBook-Pro-3.local>
2012-09-14 15:27:42 +0200

    bugfix

--- vizz_frame.php
+++ vizz_frame.php
@@ -110,7 +110,7 @@
 						<input style="width:140px" type="button" id='PlayAnimation' value="Play animation">
 						<input style="width:80px" type="button" id='Day+' value="Step+">
 						
-						| Filter name: <input type="text" id="find"/> | Highlight all connected nodes: <input type="checkbox" name="highlightConnected" id="highlightConnected"/>
+						<br>| Filter name: <input type="text" id="find"/> | Highlight all connected nodes: <input type="checkbox" name="highlightConnected" id="highlightConnected"/>
 					</div>
 				</div>
 			</div>			
